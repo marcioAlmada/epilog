3c8659f4080fc51923c67cda18f020eee779c1c9
Márcio Almada <marcio3w@gmail.com>
2014-08-16 18:13:00 -0300

    add DataBag + tests

---
+++ src/DataBag.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Epilog;
+
+class DataBag
+{
+    protected $data;
+
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    public function __get($key)
+    {
+        return $this->get($key);
+    }
+
+    public function __set($key, $value)
+    {
+        return $this->set($key, $value);
+    }
+
+    public function get($key, $default = null)
+    {
+        return \igorw\get_in($this->data, $this->makePath($key), $default);
+    }
+
+    public function set($key, $value)
+    {
+        return $this->data = \igorw\assoc_in($this->data, $this->makePath($key), $value);
+    }
+
+    public function all()
+    {
+        return $this->data;
+    }
+
+    protected function makePath($key)
+    {
+        return explode('.', str_replace(' ', '.', $key));
+    }
+}
---
+++ test/DataBagTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Epilog;
+
+/**
+ * DataBagTest
+ *
+ * @group support
+ */
+class DataBagTest extends \PHPUnit_Framework_TestCase
+{
+    protected $bag;
+
+    protected $data = [
+        'a' => [
+            'b' => [
+                'c' => 'd'
+            ]
+        ]
+    ];
+
+    public function setUp()
+    {
+        $this->bag = new DataBag($this->data);
+    }
+
+    public function testAll()
+    {
+        $this->assertEquals($this->data, $this->bag->all());
+    }
+
+    public function testSet()
+    {
+        $this->bag->set('a.b.c', 'PATCH'); // update nested index
+        $this->assertArraySubset(['a' => ['b' => ['c' => 'PATCH']]], $this->bag->all());
+
+        $this->bag->set('a.b.c.d', 'PATCH'); // override non-array value
+        $this->assertArraySubset(['a' => ['b' => ['c' => ['d' => 'PATCH']]]], $this->bag->all());
+
+        $this->bag->{'x y z'} = 'PATCH'; // create nested index
+        $this->assertArraySubset(['x' => ['y' => ['z' => 'PATCH']]], $this->bag->all());
+    }
+
+    public function testGet()
+    {
+        $this->assertEquals('d', $this->bag->get('a.b.c'));
+        $this->assertEquals('d', $this->bag->{'a b c'});
+        $this->assertEquals('default', $this->bag->get('not set', 'default'));
+    }
+}
